f21473ba471e0d2d386ecd640c230eac533d8271
Matt DeMonaco <mdemonaco@gmail.com>
2019-12-15 22:28:26 -0800

    started implementing inquirer prompts.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,10 +18,34 @@ console.log("connected as id " + connection.threadId);
 afterConnection();
 });
 
-function afterConnection() {
-    connection.query("SELECT * FROM products", function(err,res) {
-        if (err) throw err;
-        console.log(res);
-        connection.end();
-    });
+
+function(){
+    inquirer.prompt({
+        name: "IDask",
+        type: "rawlist",
+        message: "What is the ID of the product you're looking for?",
+        choices: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    }).then(function(res){
+        inquirer.prompt({
+            name: "quantityAsk",
+            type: "input",
+            message: "How much of that product would you like to buy?"
+        }).then(function(res){
+            var res = 
+
+        })
+
+    })
 }
+
+
+
+
+
+// function afterConnection() {
+//     connection.query("SELECT * FROM products", function(err,res) {
+//         if (err) throw err;
+//         console.log(res);
+//         connection.end();
+//     });
+// }
